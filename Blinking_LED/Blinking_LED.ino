4007a2e04e3f7ee7dffd078269747c5505843b11
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-06-22 08:19:22 +0900

    Add button

--- Blinking_LED/Blinking_LED.ino
+++ Blinking_LED/Blinking_LED.ino
@@ -1,12 +1,18 @@
 const int LED = 13;
+const int BUTTON = 7;
+
+int val = 0;
 
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  digitalWrite(LED, LOW);
-  delay(1000);
+  val = digitalRead(BUTTON);
+  if (val == HIGH) {
+    digitalWrite(LED, HIGH);
+  } else {
+    digitalWrite(LED, LOW);
+  }
 }
