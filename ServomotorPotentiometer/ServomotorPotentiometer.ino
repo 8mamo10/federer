2eccd5fde9544483058a1fcafc71eb1bdce8532d
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-07-02 04:34:02 +0900

    Add ServomotorPotentiometer

---
+++ ServomotorPotentiometer/ServomotorPotentiometer.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int potpin = 0;
+int val;
+
+void setup() {
+  myservo.attach(9);
+}
+
+void loop() {
+  val = analogRead(potpin);
+  val = map(val, 0, 1023, 0, 180);
+  //val = map(val, 0, 1023, 0, 90);
+  myservo.write(val);
+  delay(15);
+}
