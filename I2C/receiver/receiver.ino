83762ac9d2be8d896c54fd9b51c39ac31b0bf411
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-10-09 21:00:38 +0900

    Add I2C receiver

---
+++ I2C/receiver/receiver.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+
+const int I2C_SLAVE_ADDRESS = 0x04;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(I2C_SLAVE_ADDRESS);
+  Wire.onReceive(onRecv);
+  Serial.print("STARTUP --- OUT\n");
+}
+
+void loop() {
+}
+
+static char gBuf[255];
+
+void onRecv(int inNumOfRecvBytes) {
+  sprintf(gBuf, "NumOfBytes=%d", inNumOfRecvBytes);
+  Serial.print(gBuf);
+
+  while(Wire.available() > 0) {
+    unsigned char c = Wire.read();
+    sprintf(gBuf, "%02x ", c) ;
+    Serial.print(gBuf);
+  }
+  Serial.print("\n");
+}
